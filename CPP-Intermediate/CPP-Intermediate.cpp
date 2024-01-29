eaa75b5dfeb8989cb79dff260f71a64a1ff8b1b6
KIPKIPS <2432719656@qq.com>
2024-01-29 18:54:26 +0800

    参数传递

--- CPP-Intermediate/CPP-Intermediate.cpp
+++ CPP-Intermediate/CPP-Intermediate.cpp
@@ -5,14 +5,56 @@
 #include <string>
 using namespace std;
 
+//函数声明
 void greet();
+int sum(int a, int b);
+int reset_int(int i);
+void reset_int_ref(int & i);
+
 
 int main()
 {
-    greet();
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     greet();
+    // }
+    // string a;
+    // string b;
+    // cout << "enter first number: ";
+    // getline(cin, a);
+    // cout << "enter second number: ";
+    // getline(cin, b);
+    // cout << a << " + " << b << " = " << sum(stoi(a), stoi(b)) << endl;
+
+    //参数传递
+    int i = 10;
+    int j = reset_int(i);
+    cout << j << endl;
+    cout << i << endl;
+    reset_int_ref(i);
+    cout << i << endl;
     cin.get();
 }
 
-void greet() {
+void greet()
+{
     cout << "hello" << endl;
+    static int count = 0;
+    count++;
+    cout << "执行了" << count << "次" << endl;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int reset_int(int i)
+{
+    return i * 2;
+}
+
+void reset_int_ref(int& i)
+{
+    i *= 2;
 }
