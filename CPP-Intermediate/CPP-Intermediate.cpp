6d7e64bf217b4fbc631bbb1929c1cd6740ddf13b
KIPKIPS <2432719656@qq.com>
2024-01-29 17:07:41 +0800

    rider

---
+++ CPP-Intermediate/CPP-Intermediate.cpp
@@ -0,0 +1,18 @@
+﻿// CPP-Intermediate.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void greet();
+
+int main()
+{
+    greet();
+    cin.get();
+}
+
+void greet() {
+    cout << "hello" << endl;
+}
