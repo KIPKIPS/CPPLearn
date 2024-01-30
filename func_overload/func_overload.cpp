bef5f1a57aca5bc4e0459b7d86e9aa37561d2d34
KIPKIPS <2432719656@qq.com>
2024-01-30 14:42:27 +0800

    函数重载2

--- func_overload/func_overload.cpp
+++ func_overload/func_overload.cpp
@@ -1,8 +1,45 @@
 //函数重载
 #include <iostream>
+#define LINE_FEED cout << endl;
 using namespace std;
-int main()
+void print(const char *cp);
+void print(const int *start,const int *end);
+void print(const int array[],size_t size);
+void main()
 {
+    //1
+    char c = 'k';
+    print(&c);
+    LINE_FEED
     
-    return 0;
+    //2
+    int arr[]{1,2,3,4,5};
+    print(&arr[0],&arr[size(arr)]);
+    LINE_FEED
+    //3
+    print(arr,size(arr));
+    LINE_FEED
+    cin.get();
+}
+
+void print(const char *cp)
+{
+    cout << "1: " << *cp <<endl;
+}
+
+void print(const int *start,const int *end)
+{
+    int length = end - start;
+    for (int i = 0; i < length; ++i)
+    {
+        cout << "2: " << *(start + i) << endl;
+    }
+}
+
+void print(const int array[],size_t size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "3: " << array[i] << endl;
+    }
 }
