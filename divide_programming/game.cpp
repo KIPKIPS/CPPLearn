23f354b89bfd005f604325ad8eb95beeb32fd1a7
KIPKIPS <2432719656@qq.com>
2024-01-30 16:38:15 +0800

    分离式编程

---
+++ divide_programming/game.cpp
@@ -0,0 +1,11 @@
+#include "game.h"
+
+void input(Game games[], int size) {
+
+}
+void sort(Game games[], int size) {
+
+}
+void print(const Game games[], const int size) {
+
+}
---
+++ divide_programming/game.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifdef GAME_H_
+#define GAME_H_
+
+#include<string>
+using namespace std;
+
+struct Game
+{
+    string name;
+    float score;
+};
+
+void input(Game games[], int size);
+void sort(Game games[], int size);
+void print(const Game games[], const int size);
+
+const int Size = 5;
+#endif
